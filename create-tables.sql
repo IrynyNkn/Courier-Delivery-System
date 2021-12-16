a51e40ad4db07cdfd49a13a78160930fe80e1353
Iryyyny <75064356+IrynyNkn@users.noreply.github.com>
2021-12-16 14:08:10 +0200

    created tables

---
+++ create-tables.sql
@@ -0,0 +1,80 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    age INT NULL,
+
+    CHECK ( age > 0 AND age < 150 )
+);
+
+CREATE TABLE IF NOT EXISTS delivery_status (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS label (
+    id SERIAL PRIMARY KEY,
+    label_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS courier (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    experience INT NULL,
+
+    CHECK ( experience > 0 )
+);
+
+CREATE TABLE IF NOT EXISTS category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS commodity (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(512) NOT NULL,
+    weight INT NOT NULL,
+    price INT NOT NULL,
+    quantity INT NULL
+);
+
+CREATE TABLE IF NOT EXISTS commodity_category (
+    commodity_id INT NOT NULL,
+    category_id INT NOT NULL,
+
+    PRIMARY KEY (commodity_id, category_id),
+    FOREIGN KEY (commodity_id) REFERENCES commodity (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
+);
+
+CREATE TABLE IF NOT EXISTS commodity_label (
+    commodity_id INT NOT NULL,
+    label_id INT NOT NULL,
+
+    PRIMARY KEY (commodity_id, label_id),
+    FOREIGN KEY (commodity_id) REFERENCES commodity (id),
+    FOREIGN KEY (label_id) REFERENCES label (id)
+);
+
+CREATE TABLE IF NOT EXISTS office (
+    id SERIAL PRIMARY KEY,
+    address VARCHAR(512) NOT NULL,
+    max_weight_available INT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    commodity_id INT NOT NULL,
+    delivery_status_id INT NOT NULL,
+    office_id INT NOT NULL,
+    courier_id INT NOT NULL,
+    number_of_products INT NOT NULL,
+
+    FOREIGN KEY (commodity_id) REFERENCES commodity (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (delivery_status_id) REFERENCES delivery_status (id),
+    FOREIGN KEY (office_id) REFERENCES office (id),
+    FOREIGN KEY (courier_id) REFERENCES courier (id),
+    CHECK ( number_of_products > 0 )
+);
